9b9b185d14461eb8bbb78fcc41d4ef75a0ed151b
NG CHING YI <cyng93@gmail.com>
2015-11-23 15:24:51 +0000

    add flag to en/disable multi-classifier detection; update README.txt

--- README.txt
+++ README.txt
@@ -1,3 +1,5 @@
-SIMPLY 
-	$ cmake . && make 
+SIMPLY
+	$ cmake . && make
+        $ ln -s /home/"$USER"/Downloads testVideo
+        $ cd ~/Downloads/ && wget <AVAILABLE_SRC>/oriVideo.mov && cd -
 
--- objectDetection.cpp
+++ objectDetection.cpp
@@ -9,14 +9,15 @@
 
 #include <iostream>
 #include <stdio.h>
+#include <iomanip>
 
 //#define sourceWebcam
 #define userInput
 #define scaleInputOn        // WIDTH=16*n    HEIGHT=9*n
-#define jumpFrame 90
-#define outputCorrect
-#define numOfTolerant 3
-
+#define jumpFrame 30
+#define outputFrame
+#define numOfTolerant 2
+#define multiDetect
 
 using namespace std;
 using namespace cv;
@@ -28,8 +29,9 @@ void detectAndDisplay( Mat frame );
 //-- Note, either copy these two files from opencv/data/haarscascades to your current folder, or change these locations
 String obj_cascade_name = "classifier/frontal_pos3000_stg14.xml";
 String obj2_cascade_name = "classifier/frontal_pos3000_stg20.xml";
+String obj3_cascade_name = "classifier/frontal_pos3000_stg30.xml";
 
-CascadeClassifier obj_cascade, obj2_cascade;
+CascadeClassifier obj_cascade, obj2_cascade, obj3_cascade;
 string window_name = "Object detection";
 RNG rng(12345);
 
@@ -38,7 +40,7 @@ double totalFrameNum = 0;
 double numOfCorrectFrame = 0;
 String videoFilename = "testVideo/oriVideo.mov";
 int scaleInput = 40;
-#ifdef outputCorrect
+#ifdef outputFrame
 String outputFilePrefix  = "./outputFrame/frame_";
 String outputFileType = ".png";
 stringstream ss;
@@ -65,6 +67,7 @@ int main( int argc, char **argv  )
 #endif
     if( !obj_cascade.load( obj_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
     if( !obj2_cascade.load( obj2_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+    if( !obj3_cascade.load( obj3_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
     //-- 2. Read the video stream
 #ifdef sourceWebcam
@@ -129,14 +132,18 @@ int main( int argc, char **argv  )
  */
 void detectAndDisplay( Mat frame )
 {
-    std::vector<Rect> objs, objs2;
+    std::vector<Rect> objs, objs2, objs3;
     Mat frame_gray;
 
     cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
     equalizeHist( frame_gray, frame_gray );
     //-- Detect objs
     obj_cascade.detectMultiScale( frame_gray, objs, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+#ifdef multiDetect
     obj2_cascade.detectMultiScale( frame_gray, objs2, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+    obj3_cascade.detectMultiScale( frame_gray, objs3, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+#endif
+
 
     cout << "[" <<
 #ifdef jumpFrame
@@ -144,36 +151,50 @@ void detectAndDisplay( Mat frame )
 #else
     frameCount
 #endif
-    << "] frame #" << frameCount << " : " << endl 
-	<< " " << obj_cascade_name << ": " << objs.size() << " ; " << obj2_cascade_name << ": " << objs2.size() << endl;
+    << "] frame #" << frameCount << " : "
+    << endl << "\t" << obj_cascade_name << ": " << objs.size()
+#ifdef multiDetect
+    << endl << "\t" << obj2_cascade_name << ": " << objs2.size()
+    << endl << "\t" << obj3_cascade_name << ": " << objs3.size()
+#endif
+    << endl;
 
     for( size_t i = 0; i < objs.size() ; i++ )
     {
 	Point upperLeft_1( objs[i].x, objs[i].y );
 	Point bottomRight_1( objs[i].x + objs[i].width, objs[i].y + objs[i].height );
 	rectangle( frame, upperLeft_1, bottomRight_1, Scalar( 255, 0, 255 ), 2, 8, 0 );
-
     }
+#ifdef multiDetect
     for( size_t i = 0; i < objs2.size() ; i++ )
     {
 	Point upperLeft_2( objs2[i].x, objs2[i].y );
 	Point bottomRight_2( objs2[i].x + objs2[i].width, objs2[i].y + objs2[i].height );
 	rectangle( frame, upperLeft_2, bottomRight_2, Scalar( 255, 255, 0 ), 2, 8, 0 );
-
     }
+    for( size_t i = 0; i < objs3.size() ; i++ )
+    {
+	Point upperLeft_3( objs3[i].x, objs3[i].y );
+	Point bottomRight_3( objs3[i].x + objs3[i].width, objs3[i].y + objs3[i].height );
+	rectangle( frame, upperLeft_3, bottomRight_3, Scalar( 0, 255, 255 ), 2, 8, 0 );
+    }
+#endif
     //-- Show what you got
     //imshow( window_name, frame );
 
-    //if(objs.size() <= numOfTolerant)
-    if(objs.size())
-    {
-	numOfCorrectFrame++;
-#ifdef outputCorrect
-	ss << outputFilePrefix << frameCount << outputFileType;
-	outputFilename = ss.str();
-	ss.str("");
-	imwrite(outputFilename, frame);
+    if( objs.size() <= numOfTolerant
+    #ifdef multiDetect
+        && objs2.size() <= numOfTolerant
+        && objs3.size() <= numOfTolerant
+    #endif
+    )
+        numOfCorrectFrame++;
+#ifdef outputFrame
+    ss << outputFilePrefix << setfill('0') << setw(5) << frameCount << outputFileType;
+    outputFilename = ss.str();
+    ss.str("");
+    imwrite(outputFilename, frame);
 #endif
-    }
+
 
 }
